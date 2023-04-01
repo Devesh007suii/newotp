d6cc2de0feaedf880d12543664918d8705c530bc
Devesh Sharma <devesh.s785dds@gmail.com>
2023-04-01 15:36:05 +0530

    1st change

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
 export const connectDatabase = ()=> {
-mongoose.connect(process.env.MONGO_URI).then(()=>{console.log(`MongoDb connected`)})
+mongoose.connect("mongodb+srv://deveshs785dds:HvTjR4U9Mf2Wq7UX@cluster0.8w4hexg.mongodb.net/test").then(()=>{console.log(`MongoDb connected`)})
 }
